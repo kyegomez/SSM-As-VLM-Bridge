6ff10dbb518714e9eac3e0ab48d9505d27d2f1a0
Kye Gomez <kye@swarms.world>
2025-06-21 13:12:29 -0700

    fixed init imports

--- ssm_bridge/__init__.py
+++ ssm_bridge/__init__.py
@@ -1,4 +1,25 @@
-from ssm_bridge.model import EnhancedVLM, EnhancedVisionTransformer, EnhancedSSMBridge, VLMConfig, SSMLayer
+from ssm_bridge.model import (
+    EnhancedVLM,
+    EnhancedVisionTransformer,
+    EnhancedSSMBridge,
+    VLMConfig,
+    SSMLayer,
+    Swish,
+    SwiGLU,
+    MultiQueryAttention,
+    CrossModalAttention,
+    TransformerBlock,
+)
 
-
-__all__ = ["EnhancedVLM", "EnhancedVisionTransformer", "EnhancedSSMBridge", "VLMConfig", "SSMLayer"]
+__all__ = [
+    "EnhancedVLM",
+    "EnhancedVisionTransformer", 
+    "EnhancedSSMBridge",
+    "VLMConfig",
+    "SSMLayer",
+    "Swish",
+    "SwiGLU",
+    "MultiQueryAttention",
+    "CrossModalAttention",
+    "TransformerBlock",
+]
